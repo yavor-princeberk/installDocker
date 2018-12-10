6bd9bf66dc792057e1a5f84bf283d030d440c11a
yavor-princeberk <42776970+yavor-princeberk@users.noreply.github.com>
2018-12-10 10:40:15 +0200

    Create installDocker.sh

---
+++ installDocker.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+while true; do
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+        sudo apt-get install -y software-properties-common python-software-properties https-transport
+        sudo apt-get update
+        sudo apt-get install apt-transport-https
+        apt-cache policy docker-ce
+        echo "Press Q to exit, enter to continue \n"
+        read input
+        if [[ $input = "q" ]] || [[ $input = "Q" ]]
+                then break
+        fi
+        sudo apt-get install -y docker-ce
+        sudo systemctl status docker
+        read input
+        echo -en "Adding user deploy - Press Q to exit, enter to continue \n"
+        if [[ $input = "q" ]] || [[ $input = "Q" ]]
+                then break
+        fi
+        sudo adduser deploy
+        sudo usermod -a -G docker deploy
+        echo -en "I will reboot - Press Q to exit, enter to continue \n"
+        if [[ $input = "q" ]] || [[ $input = "Q" ]]
+                then break
+        fi
+        sudo reboot
+
+done
